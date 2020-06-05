ac128844d8c74e78b04080763f009883b71d4c88
dawwik <dawid.wiktorowski22@gmail.com>
2020-06-05 19:18:07 +0200

    Add new files

---
+++ src/com/company/Ksztalty.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface Ksztalty {
+
+    double powierzchnia();
+
+    public double obwod();
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ksztalty prostokat = new Prostokat(5.0, 8.0);
+
+        System.out.println("Powierzchnia prostokata to: " + prostokat.powierzchnia());
+    }
+}
