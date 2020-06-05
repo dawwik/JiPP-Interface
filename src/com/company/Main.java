ec0051a8beca48274dcf530f6125567cb0ad3c5d
dawwik <dawid.wiktorowski22@gmail.com>
2020-06-05 19:21:00 +0200

    Add new files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Ksztalty prostokat = new Prostokat(5.0, 8.0);
+        Ksztalty prostokat = new Prostokat(5.0, 9.0);
 
         System.out.println("Powierzchnia prostokata to: " + prostokat.powierzchnia());
     }
